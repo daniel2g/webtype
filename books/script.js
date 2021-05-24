6f42caa5f71e02b8b84efa841262fe63af891e1c
daniel2g <68967729+daniel2g@users.noreply.github.com>
2021-05-24 13:39:43 -0400

    may 24 2021

--- books/script.js
+++ books/script.js
@@ -8,8 +8,8 @@ function pileImages(){
     var i = 1;
     while (i < 27) {
 
-        let posx = Math.floor(Math.random() * 70);
-        let posy = Math.floor(Math.random() * 70);
+        let posx = Math.floor(Math.random() * 85);
+        let posy = Math.floor(Math.random() * 65);
         let zi = 27 - i;
 
         var newImg = '<img class="cover" id="file'+
@@ -20,11 +20,19 @@ function pileImages(){
         posy +
         'vh; left: calc(' +
         posx +
-        'vw + 280px); z-index:' +
+        'vw + 0px); z-index:' +
         zi +
         ';"></img>';
         
         pileImagesOutput.push(newImg);
+
+        if(i===1){
+            $(".title").css({
+                'top': posy + 'vh',
+                'left': posx + 'vw',
+                'z-index': 27
+            })
+        }
 
         i++
     }
@@ -36,10 +44,11 @@ const pileContainer = document.getElementById("pile_container")
 pileContainer.innerHTML = pileImagesOutput.join("");
 
 $(".cover").draggable();
+$(".title").draggable();
 
 var index = 27;
 
-$(".cover").each(function(item){
+$(".cover, .title").each(function(item){
     this.addEventListener("mousedown", function(e){
         $(this).css("z-index", index);
         index++;
@@ -47,14 +56,17 @@ $(".cover").each(function(item){
 });
 
 
-$(".aboutbutton").on("click", function(){
+
+
+$(".title").on("click", function(){
     $(".about").show();
     $(".small_back").show();
     $(".book").hide();
     $(".small").hide();
+    
 });
 
-$(".small_back").on("click", function(){
+$(".about").on("click", function(){
     $(".book").hide();
     $(".about").hide();
     $(".small_back").hide();
@@ -72,6 +84,9 @@ $("#file1").on("click", function(){
     $("#book1").show();
     $(".small").hide();
     $(".small_back").show();
+
+    window.open("page1.html", "_blank");
+   
     
 });
 
@@ -79,4 +94,7 @@ $("#file2").on("click", function(){
     $("#book2").show();
     $(".small").hide();
     $(".small_back").show();
+
+    window.open("page2.html", "_blank");
+    
 });
